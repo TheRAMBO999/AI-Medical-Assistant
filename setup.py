18e77784c7eeb7f8007ada9f678904d1931ac28d
TheRAMBO999 <108071249+TheRAMBO999@users.noreply.github.com>
2026-04-28 21:21:08 +0200

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AI-Medical-Assistant',    
+    version='0.0.0',
+    author='Nithees MAHALINGAM',
+    author_email='nitheesmahalingam069@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    
+)
